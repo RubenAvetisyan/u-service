f7856b9811de5e9c20ae0c28f278f535feeca49b
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-04-27 13:46:01 +0400

    target: static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
+
   publicRuntimeConfig: {
     BASE_URL: process.env.BASE_URL || 'http://localhost:3000/',
   },
