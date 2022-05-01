74357c4ee01f6088a356a07cf1dde558f759e101
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-05-02 00:29:16 +0400

    fetch composible added

---
+++ composables/notion.ts
@@ -0,0 +1,11 @@
+export const notionFetch = (url: string, fetchOptions: any = {}) => {
+  // eslint-disable-next-line no-undef
+  return $fetch(url, {
+    baseURL: '/api',
+    ...fetchOptions,
+    // headers: {
+    //   Authorization: `token ${useGithubCookie().value}`,
+    //   ...fetchOptions.headers,
+    // },
+  })
+}
