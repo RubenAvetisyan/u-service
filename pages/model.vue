447bab4aaead00b26c43c9799fbf031bf9150f07
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-05-07 01:47:35 +0400

    change chevrone style

--- pages/model.vue
+++ pages/model.vue
@@ -23,7 +23,7 @@ const scrollDown = selector => useScrollIntoParentNextSiblingElement(selector)
 <template>
   <div class="h-screen justify-between pb-13.75 flex flex-col items-center">
     <div class="snap-start h-screen pt-[calc(13vh+20px)]">
-      <div v-if="links.length" class="text-center">
+      <div id="main-content" v-if="links.length" class="text-center">
         <NuxtPage class="w-full mx-auto justify-center items-center" />
 
         <h2 class="font-light tracking-wider text-[#5c5d61] text-3.5 pb-2">
@@ -41,11 +41,11 @@ const scrollDown = selector => useScrollIntoParentNextSiblingElement(selector)
 
       <div v-if="link.services?.length" id="contents">
         <div v-for="({ name='', imgUrl='', itemClass = '', path = '/' }, i) in link.services"
-          :key="generatedKey(name + '-' + i)" :id="`content-${i}`"
-          :style="`background-image: url('${imgUrl}');`"
+          :key="generatedKey(name + '-' + i)" :id="`content-${i}`" :style="`background-image: url('${imgUrl}');`"
           :class="[bgImg, 'snap-start h-screen w-screen relative bg-cover bg-fixed bg-center', itemClass, 'flex text-center items-center justify-center']">
           <div class="flex mx-4 items-center justify-center h-10 text-center content-center">
-            <r-home-menu :key="`service-block-${path.replace(/\//g, '')}`" color="bg-[#181a1f]" routes-prefix="model" :path="path"
+            <r-home-menu :key="`service-block-${path.replace(/\//g, '')}`" color="bg-[#181a1f]" routes-prefix="model"
+              :path="path"
               class="flex mx-auto text-light-blue-100 hover:text-light-100 bg-opacity-50 h-20 w-80 ma-2 rounded-xl align-center justify-center">
               <span
                 :class="['bg-clip-text font-bold font-bold tracking-wider text-size-[0.95rem] leading-[1.15] py-1 capitalize mw-auto px-1']">
@@ -53,6 +53,10 @@ const scrollDown = selector => useScrollIntoParentNextSiblingElement(selector)
             </r-home-menu>
 
           </div>
+          <content-chevron-down
+            @click="() => scrollDown(i + 1 < link.services.length ? `#content-${i + 1}` : '#main-content')"
+            :class="['mt-[50vh] md:mt-[65vh] mb-[6vh] absolute', i + 1 >= link.services.length ? 'rotate-180' : null]">
+          </content-chevron-down>
         </div>
       </div>
     </div>
