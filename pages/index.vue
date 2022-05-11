85bd5c7753a043a072f32afbc6cf65c91768d071
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-05-12 02:49:56 +0400

    icon changed

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@ useMeta({
     <div class="snap-start h-full w-full items-center">
       <div v-if="links" class="flex flex-wrap items-center mx-auto w-full pt-15 gap-3">
         <r-home-menu v-for="({ path = '/', splitedName = '' }) in links" :key="`service-block-${splitedName.join('-')}`"
-          routes-prefix="model" :path="path" color="bg-[#181a1f]" icon="/tesla-svgrepo-com.svg"
+          routes-prefix="model" :path="path" color="bg-[#181a1f]" icon="/U-Service.png"
           class="flex mx-auto w-40 h-40 ma-2 pa-4 rounded-full">
           <template #first-line>
             {{ splitedName[0] }}
