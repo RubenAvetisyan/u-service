058dfe4506577f7d0bc5b2f891861873691c1629
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-06-05 01:01:53 +0400

    style refactored

--- components/HomeMenu.vue
+++ components/HomeMenu.vue
@@ -40,24 +40,36 @@ const onClick = () => {
 
 <template>
   <NuxtLink :to="!!path ? `${baseUrl}${routesPrefix}${path}` : '/'" :class="[bgColor]"
-    class="group flex-wrap-reverse bg-opacity-35 cursor-pointer overflow-hidden backdrop-blur-sm relative shadow-lg ring-1 ring-white/5 light:ring-black/5 items-center hover:shadow-md hover:shadow-gray-700 dark:hover:shadow-white hover:light:bg-slate-800 hover:dark:highlight-white/5 hover:bg-gradient-to-r from-transparent via-gray-500/50 to-transparent"
+    class="group flex-wrap-reverse bg-opacity-35 objcet-cover cursor-pointer overflow-hidden backdrop-blur-sm relative shadow-lg ring-1 ring-white/5 light:ring-black/5 items-center hover:shadow-md hover:shadow-gray-700 dark:hover:shadow-white hover:light:bg-slate-800 hover:dark:highlight-white/5 hover:bg-gradient-to-r from-transparent via-gray-500/50 to-transparent"
     @click.stop="onClick">
     <img v-if="background" :src="background" :alt="path"
-      class="rounded-full h-full object-cover w-full backdrop-blur-10 opacity-60 transition-all ease-in-out duration-150" width="160px"
-      height="160px">
-    <div :class="background ? 'absolute left-0' : ''">
-      <img v-if="icon"
-        class="w-6 h-6 shadow-lg rounded-full mx-auto transition duration-800 delay-75 ease-in-out group-hover:scale-125 group-hover:animate-bounce group-hover:grayscale shadow-lg shadow-[#0080BF] group-hover:invert"
-        :src="icon" width="120px" height="120px">
-      <div
-        class="flex flex-col text-center align-middle pa-5 w-full justify-center content-center items-center transition duration-600 ease-in-out group-hover:scale-115">
-        <slot v-if="!$slots['first-line'] && !$slots['secont-line']" />
-        <span v-if="$slots['first-line']" class="text-light-50 text-sm font-medium font-bold dark:text-slate-200">
-          <slot name="first-line" as="template" />
-        </span>
-        <span v-if="$slots['second-line']" class="text-light-10 text-sm font-medium dark:text-slate-400">
-          <slot name="second-line" />
-        </span>
+      class="rounded-full object-cover h-full w-full backdrop-blur-10 opacity-70 dark:opacity-50 group-hover:opacity-50 transition-all ease-in-out duration-150"
+      width="180px" height="180px">
+    <div :class="background ? 'absolute left-0 place-content-center w-full h-full' : ''">
+      <div class="relative w-full h-full">
+        <div class="w-full h-full absolute z-10 mt-10 place-content-center mx-auto items-center">
+          <img v-if="icon"
+            class="w-6 h-6 shadow-lg rounded-full mx-auto transition duration-800 delay-75 ease-in-out group-hover:scale-125 group-hover:animate-bounce group-hover:grayscale shadow-lg shadow-[#0080BF] group-hover:invert"
+            :src="icon" width="120px" height="120px">
+          <div
+            class="flex flex-col tracking-tight text-center place-content-center align-middle pa-5 w-full justify-center content-center items-center transition duration-600 ease-in-out group-hover:scale-112">
+            <slot v-if="!$slots['first-line'] && !$slots['secont-line']" />
+            <span v-if="$slots['first-line']" class="text-light-700 text-sm font-medium font-bold dark:text-light-300">
+              <slot name="first-line" as="template" />
+            </span>
+            <span v-if="$slots['second-line']"
+              class="text-light-10 text-sm font-medium text-stone-700 group-hover:text-slate-300 dark:text-light-900/80">
+              <slot name="second-line" />
+            </span>
+          </div>
+        </div>
+
+        <div
+          class="absolute z-0 flex top-0 left-0 place-content-center mx-auto items-center justify-center w-full h-full">
+          <div
+            class="rounded-full h-[9rem] w-[9rem] backdrop-blur-10 opacity-70 dark:opacity-50 group-hover:opacity-50 transition-all ease-in-out duration-150">
+          </div>
+        </div>
       </div>
 
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -24,12 +24,12 @@ useHead({
   <div class="relative snap-center h-full justify-between w-full x-overflow-hidden items-center px-2 md:px-4">
     <RSlider :images="sliderSrc" class="absolute top-16" />
     <Suspense>
-      <div class="flex snap-start w-full items-center pt-16 md:pt-0 md:mt-[18vh]">
+      <div class="flex snap-start w-full items-center pt-16 md:pt-0 md:mt-[18vh] lg:mt-[10vh]">
         <div v-if="links" class="flex flex-wrap items-center mx-auto w-full pt-15 gap-3">
           <r-home-menu v-for="({ path = '/', splitedName = '', imgUrl = '' }, key) in links"
             :key="`service-block-${splitedName.join('-')}-${key}`" routes-prefix="model" :path="path"
             icon="/U-Service.png" :background="imgUrl" color="bg-[#181a1f]"
-            class="flex mx-auto w-40 h-40 ma-2 pa-4 rounded-3xl lg:rounded-full">
+            class="flex mx-auto w-40 h-40 mx-2 pa-2 rounded-3xl lg:rounded-full">
             <template #first-line>
               {{ splitedName[0] }}
             </template>
