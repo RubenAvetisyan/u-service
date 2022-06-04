c0c18e3683bbc7f3b28074f7f2db18645bc0a185
Ruben Avetisyan <42075410+RubenAvetisyan@users.noreply.github.com>
2022-06-05 01:12:21 +0400

    style refactoring

--- components/HomeMenu.vue
+++ components/HomeMenu.vue
@@ -47,18 +47,19 @@ const onClick = () => {
       width="180px" height="180px">
     <div :class="background ? 'absolute left-0 place-content-center w-full h-full' : ''">
       <div class="relative w-full h-full">
-        <div class="w-full h-full absolute z-10 mt-10 place-content-center mx-auto items-center">
+        <div class="w-full h-full absolute z-10 mt-6 place-content-center mx-auto items-center">
           <img v-if="icon"
             class="w-6 h-6 shadow-lg rounded-full mx-auto transition duration-800 delay-75 ease-in-out group-hover:scale-125 group-hover:animate-bounce group-hover:grayscale shadow-lg shadow-[#0080BF] group-hover:invert"
             :src="icon" width="120px" height="120px">
           <div
             class="flex flex-col tracking-tight text-center place-content-center align-middle pa-5 w-full justify-center content-center items-center transition duration-600 ease-in-out group-hover:scale-112">
             <slot v-if="!$slots['first-line'] && !$slots['secont-line']" />
-            <span v-if="$slots['first-line']" class="text-light-700 text-sm font-medium font-bold dark:text-light-300">
+            <span v-if="$slots['first-line']"
+              class="text-sm font-medium font-bold text-dark-200 group-hover:text-light-500 group-hover:dark:text-dark-500 dark:text-light-300">
               <slot name="first-line" as="template" />
             </span>
             <span v-if="$slots['second-line']"
-              class="text-light-10 text-sm font-medium text-stone-700 group-hover:text-slate-300 dark:text-light-900/80">
+              class="text-light-10 text-sm font-medium text-dark-300  group-hover:text-light-600 group-hover:dark:text-dark-600 dark:text-light-400">
               <slot name="second-line" />
             </span>
           </div>
@@ -67,7 +68,7 @@ const onClick = () => {
         <div
           class="absolute z-0 flex top-0 left-0 place-content-center mx-auto items-center justify-center w-full h-full">
           <div
-            class="rounded-full h-[9rem] w-[9rem] backdrop-blur-10 opacity-70 dark:opacity-50 group-hover:opacity-50 transition-all ease-in-out duration-150">
+            class="rounded-full h-[9rem] w-[9rem] bg-light-50 group-hover:bg-light-800 dark:bg-dark-50  opacity-50 dark:opacity-70 transition-all ease-in-out duration-150">
           </div>
         </div>
       </div>
